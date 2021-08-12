7b5c4ef4a08a4ab78e9da60570cde46281671e30
z <haig8@msn.com>
2021-08-12 22:20:20 +0800

    test: add test

---
+++ src/Field.test.tsx
@@ -0,0 +1,50 @@
+import { cleanup, render } from '@testing-library/react';
+import { Field } from './Field';
+import { Group } from './Group';
+import { createGroupContext } from './GroupContext';
+
+describe('Field', () => {
+  afterEach(cleanup);
+  it('render', () => {
+    const context = createGroupContext();
+    const { container } = render(
+      <Group context={context}>
+        <Field path="name">
+          <input />
+        </Field>
+      </Group>,
+    );
+    const inputs = container.querySelectorAll('input');
+
+    expect(inputs.length).toBe(1);
+    expect(inputs[0].value).toBe('');
+  });
+
+  it('render children', () => {
+    const context = createGroupContext();
+    const { container } = render(
+      <Group context={context}>
+        <Field path="name">{() => <input />}</Field>
+      </Group>,
+    );
+    const inputs = container.querySelectorAll('input');
+
+    expect(inputs.length).toBe(1);
+    expect(inputs[0].value).toBe('');
+  });
+
+  it('render with initial', () => {
+    const context = createGroupContext();
+    const { container } = render(
+      <Group context={context}>
+        <Field path="name" initial="value">
+          <input />
+        </Field>
+      </Group>,
+    );
+    const inputs = container.querySelectorAll('input');
+
+    expect(inputs.length).toBe(1);
+    expect(inputs[0].value).toBe('value');
+  });
+});
