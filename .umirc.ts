b7017a7a8adc09dd4313ef41904df0d691911531
z <haig8@msn.com>
2021-08-18 21:51:34 +0800

    chore: update project

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'dumi';
+import pkg from './package.json';
 
 export default defineConfig({
-  title: 'kltk-field',
+  title: pkg.name,
+  description: pkg.description,
   favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
+  mfsu: {
+    development: {
+      output: './node_modules/.cache/.mfsu-dev',
+    },
+  },
   // more config: https://d.umijs.org/config
 });
